af1736aa6cd2fa310e692e60fa4097f0d7c23c42
shenjie <shenjie@yunhx.net>
2020-08-05 15:26:41 +0800

    first commit

---
+++ src/main/java/com/shenjies88/oauth2/Oauth2PracticeApplication.java
@@ -0,0 +1,47 @@
+package com.shenjies88.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class Oauth2PracticeApplication extends WebSecurityConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2PracticeApplication.class, args);
+    }
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("login"));
+    }
+
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
+                .logout(l -> l.logoutSuccessUrl("/").permitAll())
+                .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(o -> o.failureHandler((request, response, exception) -> request.getSession().setAttribute("error.message", exception.getMessage())));
+    }
+}
---
+++ src/test/java/com/shenjies88/oauth2/Oauth2PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenjies88.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
